3fe931229fa106acfa755d4cf1213d0a4e37ab0d
PJRaj <vijayrajshanmugarajt@gmail.com>
2019-04-02 00:53:34 +0530

    A

--- proxy/newserver.py
+++ proxy/newserver.py
@@ -3,37 +3,42 @@ from time import sleep
 from urllib.request import urlopen
 from http.server import SimpleHTTPRequestHandler
 
-PORT = 20100
+PORT = 20101
 
 class Proxy(SimpleHTTPRequestHandler):
-    def do_GET(self):
-        client_addr = self.client_address
-        print(">> PROXY_SERVER : ", client_addr)
+    def handle(self):
         print(" PROXY_SERVER : Handling Client ", str(self.client_address))
-        
-        # client_addr is a tuple consisting of the ip address and socket number
-        # of the client. Using this, write a condition for allowing requests only
-        # from appropriate socket numbers
-        request_data = self.request.recv(1024).strip()
-
-        
-        self.copyfile(urlopen(self.path), self.wfile)
-        print("SUCCESS")
+        self.data = self.request.recv(1024).strip()
+        print(self.data)
 
 
-    def do_POST(self):
-        client_addr = self.client_address
-        print(">> PROXY_SERVER : ", client_addr)
-        print(" PROXY_SERVER : Handling Client ", str(self.client_address))
+class ProxyServer(socketserver.ThreadingTCPServer):
+    def __init__(self, server_address, handler_class = Proxy):
+        print('__init__')
+        socketserver.TCPServer.__init__(self, server_address, handler_class)
+        return
 
-        self.copyfile(urlopen(self.path), self.wfile)
-        print("SUCCESS")
+    def server_activate(self):
+        # print('server_activate')
+        socketserver.TCPServer.server_activate(self)
+        return
 
+    def handle_request(self):
+        return socketserver.TCPServer.handle_request(self)
+
+    def verify_request(self, request, client_address):
+        # print('verify_request(%s, %s)', request, client_address)
+        return socketserver.TCPServer.verify_request(self, request, client_address)
+
+    def process_request(self, request, client_address):
+        # print('process_request(%s, %s)', request, client_address)
+        return socketserver.TCPServer.process_request(self, request, client_address)
 
 if __name__ == "__main__":
     try:
         print(">> PROXY_SERVER : Proxy server running")
-        httpd = socketserver.ThreadingTCPServer(('', PORT), Proxy)
+        httpd = ProxyServer(('', PORT), Proxy)
+
         print(">> PROXY_SERVER : Serving at port", PORT)
 
         # Start a thread with the server -- that thread will then start one
@@ -43,6 +48,7 @@ if __name__ == "__main__":
         # Exit the server thread when the main thread terminates
         server_thread.daemon = True
         server_thread.start()
+
         try:
             while True:
                 sleep(1)
