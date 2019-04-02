bce71a07e7a479eac8c6867c28444d3a735a84a3
PJRaj <vijayrajshanmugarajt@gmail.com>
2019-04-02 20:38:06 +0530

    A

--- proxy/server.py
+++ proxy/server.py
@@ -6,20 +6,21 @@ from http.server import SimpleHTTPRequestHandler
 PORT = 20100
 
 class Proxy(SimpleHTTPRequestHandler):
+    # Get request handler (add caching code in this function)
     def do_GET(self):
-        client_addr = self.client_address
-        print(">> PROXY_SERVER : ", client_addr)
-        print(" PROXY_SERVER : GET : Handling Client ", str(self.client_address))
-        
-        # client_addr is a tuple consisting of the ip address and socket number
-        # of the client. Using this, write a condition for allowing requests only
-        # from appropriate socket numbers
+        try:
+            client_addr = self.client_address
+            print(">> PROXY_SERVER : ", client_addr)
+            print(" PROXY_SERVER : GET : Handling Client ", str(self.client_address))
+            print("Thread Name:{}".format(threading.current_thread().name))
 
-        # request_data = self.request.recv(1024).strip()
-        # print(request_data)
-        self.copyfile(urlopen(self.path), self.wfile)
-        print("SUCCESS")
-
+            self.copyfile(urlopen(self.path), self.wfile)
+            print("SUCCESS")
+        except:
+            print("\n>> PROXY_SERVER : __SIGINT__ detected")
+            print(">> PROXY_SERVER : << Shutting down >>")
+            exit()
+            return
 
     def do_POST(self):
         client_addr = self.client_address
@@ -31,39 +32,22 @@ class Proxy(SimpleHTTPRequestHandler):
 
 
 if __name__ == "__main__":
+    socketserver.ThreadingTCPServer.allow_reuse_address = True
+    httpd = socketserver.ThreadingTCPServer(('', PORT), Proxy)
     try:
         print(">> PROXY_SERVER : Proxy server running")
-        httpd = socketserver.ThreadingTCPServer(('', PORT), Proxy)
         print(">> PROXY_SERVER : Serving at port", PORT)
-
-        # Start a thread with the server -- that thread will then start one
-        # more thread for each request
-        server_thread = threading.Thread(target=httpd.serve_forever)
-
-        # Exit the server thread when the main thread terminates
-        server_thread.daemon = True
-        server_thread.start()
-        try:
-            while True:
-                sleep(1)
-
-        except KeyboardInterrupt:
-            print("\n>> PROXY_SERVER : __SIGINT__ detected")
-            print(">> PROXY_SERVER : << Shutting down >>")
-            httpd.shutdown()
-
-        except Exception as _exc:
-            print("\n>> PROXY_SERVER : _Exception_\n", _exc)
-            print(">> PROXY_SERVER : << Shutting down >>")
-            httpd.shutdown()
+        httpd.serve_forever()
 
     except KeyboardInterrupt:
         print("\n>> PROXY_SERVER : __SIGINT__ detected")
         print(">> PROXY_SERVER : << Shutting down >>")
+        httpd.server_close()
         httpd.shutdown()
 
     except Exception as _exc:
         print("\n>> PROXY_SERVER : _Exception_\n",_exc)
         print(">> PROXY_SERVER : << Shutting down >>")
+        httpd.server_close()
         httpd.shutdown()
 
